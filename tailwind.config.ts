705d1f4b9d96adf354fe2d856b4480811ffe9aea
HAMMAD ALI <hammadali.6023@gmail.com>
2024-11-21 15:53:52 +0500

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,23 +2,18 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}", // Apke project ke pages folder
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}", // Components folder
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}", // App folder (Next.js)
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: "class", // Dark mode ko manually "class" ya system-based "media" kar sakte hain
   theme: {
     extend: {
       colors: {
-        // Custom colors for light and dark mode
-        background: "#f8f9fa", // Light mode background color
-        foreground: "#212529", // Light mode text color
-        darkBackground: "#1a202c", // Dark mode background color
-        darkForeground: "#e2e8f0", // Dark mode text color
+        background: "var(--background)",
+        foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [], // Optional plugins agar aap add karna chahein
+  plugins: [],
 };
-
 export default config;
